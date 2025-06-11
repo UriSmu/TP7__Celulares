33d3e1271efed02e7aff6fe05109de87509a9fc4
UriSmu <48240113@ort>
2025-06-11 09:05:01 -0300

    Layout

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -8,12 +8,20 @@ import QuienesSomos from './src/components/QuienesSomos'
 const Layout = () => {
   return (
     <>
+    <header>
       <nav>
         <Link to="/home">Home</Link>
         <Link to="/productos">Productos</Link>
         <Link to="/quienes-somos">Quienes Somos</Link>
         <Link to="/contacto">Contacto</Link>
       </nav>
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+      <footer>
+        <h4>Esto es un footer!</h4>
+      </footer>
     </>
   );
 };
